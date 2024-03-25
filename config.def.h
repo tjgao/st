fdc0747bb6854527b85cc9ef56e12d9d3b980da3
Tiejun Gao <terry.tiejun.gao@gmail.com>
2024-03-25 18:26:49 +1100

    minor ui tweak

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:size=22:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:size=20:antialias=true:autohint=true";
 //static char *font = "TerminessTTF Nerd Font:size=18:antialias=true:autohint=true";
 static int borderpx = 10;
 
